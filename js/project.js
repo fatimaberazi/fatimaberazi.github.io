10f7b479e447c74c30136fb6ae341ee2031e5fe2
Fatima Berazi <fatimaberaze1997@gmail.com>
2017-04-05 15:16:07 +0300

    Add JQ code to project file to make favourite and current file

--- js/project.js
+++ js/project.js
@@ -1,19 +1,47 @@
-$(document).ready()(function() {
-
- $.get(url,function())
-	
+$(function() {
+  $('#favourite-repos').hide()
+  bindFavouriteBtn();
+	callRepos("https://api.github.com/users/fatimaberazi/repos?sort=pushed&per_page=5")
+})
+function bindFavouriteBtn(){
+  $('#favourite-btn').on('click',function(){
+    console.log(this)
+    //$('#favourite-repos').attr("visibility", "visible");
+    $('#favourite-repos').show()
+  })
 }
+ function callRepos(url){
+  $('#repos').html(null)
+    $("#current-btn").on('click',function(){
+      $.get(url, function(data){
+        $.each(data,function(key,v){
+        var date=new Date(v.created_at);
+        $('#repos').append(`<li><a href="${v.html_url}">${v.name} ${date}</li>`)
+               
+      })
+    });
+  });
 
-/* <div class="container">
-     <div class="row">
-       <div class="col-md-6">
-         some stuff here
-       </div>
-       <div class="col-md-6">
-         some more stuff here
-       </div>
-     </div>
-   </div>
+}
+// $('#result').append('<a href='+`${itm['html_url']}`+'>'+`${itm['name']}`+'</a><br/><br>'+`${itm['created_at']}`);
 
-   Bootstrap is built on a 12 column grid. Each row should add up to 12 columns.
-*/
+
+// function formatDate(date) {
+//   var hours = date.getHours();
+//   var monthIndex = date.getMonth();
+//   var minutes = date.getMinutes();
+//     var monthNames = [
+//     "January", "February", "March",
+//     "April", "May", "June", "July",
+//     "August", "September", "October",
+//     "November", "December"
+//   ];
+//   var ampm = hours >= 12 ? 'pm' : 'am';
+//   hours = hours % 12;
+//   hours = hours ? hours : 12; // the hour '0' should be '12'
+//   minutes = minutes < 10 ? '0'+minutes : minutes;
+//   var strTime = hours + ':' + minutes + ' ' + ampm;
+//   return  date.getDate() + "/" + monthNames[monthIndex] + "/" + date.getFullYear() + "   Time: " + strTime;
+
+// }
+
