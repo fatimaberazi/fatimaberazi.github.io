8524ae3e06d3fa7e9b46775182d86a5c0aecdabe
Fatima Berazi <fatimaberaze1997@gmail.com>
2017-04-12 10:31:47 +0300

    change the design and add spotify api

--- js/project.js
+++ js/project.js
@@ -1,3 +1,4 @@
+var isShown=false;
 $(function() {
   $('#favourite-repos').hide()
   bindFavouriteBtn();
@@ -6,8 +7,7 @@ $(function() {
 function bindFavouriteBtn(){
   $('#favourite-btn').on('click',function(){
     console.log(this)
-    //$('#favourite-repos').attr("visibility", "visible");
-    $('#favourite-repos').show()
+    $('#favourite-repos').slideToggle()
   })
 }
 function callRepos(url){
@@ -20,32 +20,13 @@ function callRepos(url){
 
         var date=new Date(v.created_at);
 
-        $('#repos').append(`<li><a href="${v.html_url}">${v.name} ${date}</li>`)
+        $('#repos').append(`☯ <a href="${v.html_url}">${v.name} <br><br>`)
 
       })
+      isShown ? $('#repos').hide('slow') : $('#repos').show('slow')
+      isShown = !isShown
     });
   });
 
 }
-// $('#result').append('<a href='+`${itm['html_url']}`+'>'+`${itm['name']}`+'</a><br/><br>'+`${itm['created_at']}`);
-
-
-// function formatDate(date) {
-//   var hours = date.getHours();
-//   var monthIndex = date.getMonth();
-//   var minutes = date.getMinutes();
-//     var monthNames = [
-//     "January", "February", "March",
-//     "April", "May", "June", "July",
-//     "August", "September", "October",
-//     "November", "December"
-//   ];
-//   var ampm = hours >= 12 ? 'pm' : 'am';
-//   hours = hours % 12;
-//   hours = hours ? hours : 12; // the hour '0' should be '12'
-//   minutes = minutes < 10 ? '0'+minutes : minutes;
-//   var strTime = hours + ':' + minutes + ' ' + ampm;
-//   return  date.getDate() + "/" + monthNames[monthIndex] + "/" + date.getFullYear() + "   Time: " + strTime;
-
-// }
-
+// ${date}
--- js/skills.js
+++ js/skills.js
@@ -1,1 +1,17 @@
-req.js
+// python -m SimpleHTTPServer 8000
+
+var url="skills.json"
+function makeAjaxRequest(){
+	$.get(url, function(data){
+		var html=''
+		$.each(data.skills,function(key,v){
+			html+=`<h1>${v.name}</h1>`
+			html+=`<p>${v.description}</p>`
+		})
+		$('#result').html(html)
+
+	});
+}
+$(document).ready(function(){
+	makeAjaxRequest();
+})
